deec64fd304963dee385f0c6ab4fba3708e731d5
Thomas <tsdexter@gmail.com>
2019-02-05 16:03:41 -0500

    make more helpful

--- nosync.js
+++ nosync.js
@@ -3,22 +3,66 @@ const shell = require("shelljs");
 const minimist = require("minimist");
 
 const args = minimist(process.argv.slice(2));
+let step = 1;
 
-// npm install
-if (args.install || args.i) {
+shell.echo("\n🖕 iCloud: Congratulations iCloud is being fixed. Thx Apple 🙄");
+
+// npm install if node_modules doesn't exist
+const sym = shell.exec("find node_modules -type l", { silent: true }).stdout;
+const install = shell.exec("find node_modules -type d", { silent: true })
+  .stdout;
+if (install === "" && sym === "") {
+  shell.echo(
+    "\n🖕 iCloud - Step " +
+      step +
+      ": Can't find node_modules - running `npm install`\n"
+  );
+  step++;
   shell.exec("npm install");
+  shell.echo("\n🖕 iCloud - Step " + step + ": Done installing packages");
+  step++;
 }
 
-const sym = shell.exec("find node_modules -type l", { silent: true }).stdout;
-
 // if symlink not exists do the stuff
-if (sym === "") {
+const nosync = shell.exec("find node_modules.nosync -type d", { silent: true })
+  .stdout;
+if (nosync === "") {
+  shell.echo(
+    "\n🖕 iCloud - Step " +
+      step +
+      ": modifying node_modules to node_modules.nosync"
+  );
+  step++;
   shell.exec("mv node_modules node_modules.nosync");
+  shell.echo(
+    "\n🖕 iCloud - Step " +
+      step +
+      ": adding node_modules -> node_modules.nosync symlink "
+  );
+  step++;
   shell.exec("ln -s node_modules.nosync node_modules");
 }
 
 // add to gitignore maybe
-if (!args.n) {
+const nogitline =
+  shell.exec(
+    'find .gitignore -type f -print0 | xargs -0 grep -l "# ignore node_modules symlink"',
+    { silent: true }
+  ).stdout === "";
+if (!args.n && nogitline) {
+  shell.echo(
+    "\n🖕 iCloud - Step " +
+      step +
+      ": Modifying .gitignore to ignore the node_modules symlink"
+  );
+  step++;
   shell.exec('echo "# ignore node_modules symlink" >> .gitignore');
   shell.exec("find node_modules -type l >> .gitignore");
 }
+
+// done
+shell.echo("\n.");
+shell.echo("\n.");
+shell.echo("\n.");
+shell.echo("\n🖕 iCloud - Step " + step + ": Profit! 💰\n");
+step++;
